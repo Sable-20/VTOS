9fc9f5afecfc50baf00638dec53acc5980f71963
Sable Ayala <sable262021@gmail.com>
2024-12-02 09:44:27 -0500

    🎨 Rename and restructure
    Renamed and restructered project so that things are more organized and more built out for a long term project

---
+++ kernel/src/include/mem/mem.h
@@ -0,0 +1,13 @@
+#ifndef MEM_H_
+#define MEM_H_
+// GCC and Clang reserve the right to generate calls to the following
+// 4 functions even if they are not directly called.
+// Implement them as the C specification mandates.
+// DO NOT remove or rename these functions, or stuff will eventually break!
+
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *s, int c, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+
+#endif
